304759bc21c8c1534c24979c28534b5c5a23af24
CieranW <cieranwong@gmail.com>
2024-09-19 15:12:29 -0700

    Completed class activity: arrays

--- ClassActivities/19_09/Array.java
+++ ClassActivities/19_09/Array.java
@@ -5,10 +5,36 @@ public class Array {
         Scanner sc = new Scanner(System.in);
         int itemCount = sc.nextInt();
 
-        int[] items = new int[itemCount];
+        double[] itemPrice = new double[itemCount];
 
         for (int i = 0; i < itemCount; i++) {
-            items[i] = sc.nextInt();
+            itemPrice[i] = sc.nextDouble();
         }
+
+        sc.close();
+
+        double totalPrice = 0;
+
+        for (int i = 0; i < itemCount; i++) {
+            totalPrice += itemPrice[i];
+        }
+
+        System.out.println("Total price: " + totalPrice);
+        
+        double maxPrice = itemPrice[0];
+        double minPrice = itemPrice[0];
+
+        for (int i = 1; i < itemCount; i++) {
+            if (itemPrice[i] > maxPrice) {
+                maxPrice = itemPrice[i];
+            }
+
+            if (itemPrice[i] < minPrice) {
+                minPrice = itemPrice[i];
+            }
+        }
+
+        System.out.println("Max price: " + maxPrice);
+        System.out.println("Min price: " + minPrice);
     }
 }
