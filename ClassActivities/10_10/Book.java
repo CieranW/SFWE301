7d0d31639180a98b2c19d792f70c056ff68badf0
Cieran Wong <95898976+CieranW@users.noreply.github.com>
2024-10-10 14:15:25 -0700

    Init commit for Book class, class activity 10/10

---
+++ ClassActivities/10_10/Book.java
@@ -0,0 +1,36 @@
+public class Book {
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isBorrowed = false;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean getIsBorrowed() {
+        return isBorrowed;
+    }
+
+    public void borrowBook() {
+        isBorrowed = true;
+    }
+
+}
