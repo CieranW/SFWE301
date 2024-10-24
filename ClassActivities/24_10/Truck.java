7ac54598b9add1e7666b43e4944427c943962570
CieranW <cieranwong@gmail.com>
2024-10-24 14:18:18 -0700

    Init commit Truck.java, child of Vehicle

---
+++ ClassActivities/24_10/Truck.java
@@ -0,0 +1,22 @@
+public class Truck extends Vehicle{
+    private int payloadCapacity;
+
+    public Truck(String make, String model, int year, int payloadCapacity) {
+        super(make, model, year);
+        this.payloadCapacity = payloadCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Payload Capacity: " + payloadCapacity + " kg");
+    }
+
+    public int getPayloadCapacity() {
+        return payloadCapacity;
+    }
+
+    public void setPayloadCapacity(int payloadCapacity) {
+        this.payloadCapacity = payloadCapacity;
+    }
+}
