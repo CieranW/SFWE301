cdc96230ced4340a9ea83da892e5358017b6d557
CieranW <cieranwong@gmail.com>
2024-10-24 14:10:59 -0700

    Include methods to print, get and set

--- ClassActivities/24_10/Vehicle.java
+++ ClassActivities/24_10/Vehicle.java
@@ -8,4 +8,34 @@ public class Vehicle {
         this.model = model;
         this.year = year;
     }
+
+    public void printVehicle() {
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
